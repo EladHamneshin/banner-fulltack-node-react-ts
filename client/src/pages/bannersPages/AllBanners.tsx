f41d9a79a8fb2dbb96963fa6a2aef7109036f9f0
pinchassh <141713748+pinchassh@users.noreply.github.com>
2023-11-26 17:06:22 +0200

    dialog for BannerByUserID

--- client/src/pages/bannersPages/AllBanners.tsx
+++ client/src/pages/bannersPages/AllBanners.tsx
@@ -1,59 +1,68 @@
-import { Box, Stack, Typography } from '@mui/material'
-import { useEffect, useState } from 'react'
-import { getAllBannersImage } from '../../api/banners/bannersImageFunc'
-import { ResponseBanner } from '../../types/BannerInterface'
-import CardBanner from '../../components/cards/CardBanner'
-import { useNavigate } from 'react-router-dom'
-
-
-
+import { useEffect, useState } from 'react';
+import { Box, Stack, Typography, CircularProgress } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { getAllBannersImage } from '../../api/banners/bannersImageFunc';
+import CardBanner from '../../components/cards/CardBanner';
+import { ResponseBanner } from '../../types/BannerInterface';
 
 const AllBanners = () => {
-    const [message, setMessage] = useState('')
-    const [banners, setBanners] = useState<ResponseBanner[] | string>([]);
-
+    const [message, setMessage] = useState('');
+    const [banners, setBanners] = useState<ResponseBanner[] | string | null>(null);
+    const [loading, setLoading] = useState(true);
     const navigate = useNavigate();
-    const handelClickLogin = () => { navigate(`/login`) }
+
+    const handleLoginRedirect = () => {
+        navigate('/login');
+    };
+
     useEffect(() => {
-        if (localStorage.getItem('token') === null) { handelClickLogin() }
-    }, [])
+        if (localStorage.getItem('token') === null) {
+            handleLoginRedirect();
+        }
+    }, []);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const result = await getAllBannersImage();
-                if (result.success === false) { return setMessage(result.message) }
-                if (result.success === true) {
-                    const data: ResponseBanner[] = result.data
-                    data.length === 0 ? setBanners('There is no such Banners') : setBanners(data)
+                if (result.success === false) {
+                    setMessage(result.message);
+                } else if (result.success === true) {
+                    const data: ResponseBanner[] = result.data;
+                    setBanners(data.length === 0 ? 'There are no banners' : data);
                 }
             } catch (error) {
-                console.log('err:', error);
+                console.error('Error fetching banners:', error);
+                setMessage('Error fetching banners');
+            } finally {
+                setLoading(false);
             }
         };
+
         fetchData();
     }, []);
 
-
     return (
         <Box>
-            {message ? (
-                <Stack>{message}</Stack>
+            {loading ? (
+                <CircularProgress />
+            ) : message ? (
+                <Stack>
+                    <Typography variant="h3">{message}</Typography>
+                </Stack>
+            ) : banners === null ? (
+                <Typography variant="h3">Loading...</Typography>
+            ) : typeof banners === 'string' ? (
+                <Typography variant="h3">{banners}</Typography>
             ) : (
-                <>
-
-                    {typeof banners === 'string' ? (
-                        <Typography variant='h3'>{banners}</Typography>
-                    ) : (
-                        banners.map((banner, index) => (
-                            <Stack key={index}><CardBanner banner={banner} /></Stack>
-                        ))
-                    )}
-
-                </>
+                banners.map((banner, index) => (
+                    <Stack key={index} spacing={2}>
+                        <CardBanner banner={banner} />
+                    </Stack>
+                ))
             )}
         </Box>
-    )
-}
+    );
+};
 
-export default AllBanners
+export default AllBanners;
--- client/src/pages/bannersPages/BannerByUserID.tsx
+++ client/src/pages/bannersPages/BannerByUserID.tsx
@@ -1,65 +1,80 @@
-import { useEffect, useState } from 'react'
-import { bannerByUserID } from '../../api/banners/bannerByUserID'
-import { Box, Stack } from '@mui/system'
-
-import { useNavigate } from 'react-router-dom'
-import CardBanner from '../../components/cards/CardBanner'
-import { ResponseBanner } from '../../types/BannerInterface'
-import { Typography } from '@mui/material'
-
+import { CircularProgress, Typography } from "@mui/material";
+import { Box, Stack } from "@mui/system";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { bannerByUserID } from "../../api/banners/bannerByUserID";
+import CardBanner from "../../components/cards/CardBanner";
+import { ResponseBanner } from "../../types/BannerInterface";
+import BannerNotFind from "./BannerNotFind";
 
 const BannerByUserID = () => {
-
     const navigate = useNavigate();
-    const handelClickLogin = () => { navigate(`login`) }
+    const handleClickLogin = () => {
+        navigate('login');
+    };
+
     useEffect(() => {
-        if (localStorage.getItem('token') === null) { handelClickLogin() }
-    }, [])
+        if (localStorage.getItem('token') === null) {
+            handleClickLogin();
+        }
+    }, []);
 
-    const [message, setMessage] = useState('')
+    const [message, setMessage] = useState('');
     const [banners, setBanners] = useState<ResponseBanner[] | string>([]);
-    const userID = localStorage.getItem('userID')
+    const userID = localStorage.getItem('userID');
 
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const result = await bannerByUserID(userID!);
+                if (userID !== null) {
+                    const result = await bannerByUserID(userID);
 
-                if (result.success === false) { return setMessage(result.message) }
-                if (result.success === true) {
-                    const data: ResponseBanner[] = result.data
-                    data.length === 0 ? setBanners('There are no banners on your name') : setBanners(data)
+                    if (result.success === false) {
+                        setMessage(result.message);
+                    } else if (result.success === true) {
+                        const data: ResponseBanner[] = result.data;
+                        setBanners(data.length === 0 ? '' : data);
+                    }
                 }
             } catch (error) {
-                console.log(error);
-
+                console.error('Error fetching banners:', error);
+                setMessage('Error fetching banners');
+            } finally {
+                setLoading(false);
             }
         };
 
         fetchData();
-
-    }, []);
-
+    }, [userID]);
 
     return (
         <Box>
-            {message ? (
-                <Stack>{message}</Stack>
+            {loading ? (
+                <Stack justifyContent="center" alignItems="center" height="100vh">
+                    <CircularProgress />
+                </Stack>
+            ) : message ? (
+                <Stack>
+                    <Typography variant="h3" textAlign="center" color="error">{message}</Typography>
+                </Stack>
             ) : (
-                <> {typeof banners === 'string' ? (
-                    <Typography variant='h3'>{banners}</Typography>
-                ) : (
-                    banners.map((banner, index) => (
-                        <Stack key={index}><CardBanner banner={banner} /></Stack>
-                    ))
-                )}
-                </>
+                <Stack spacing={2}>
+                    {typeof banners === 'string' ? (<Box>
+                        {/* <Typography variant="h3" textAlign="center">{banners}</Typography> */}
+                        <BannerNotFind />
+
+                    </Box>
+                    ) : (
+                        banners.map((banner, index) => (
+                            <CardBanner key={index} banner={banner} />
+                        ))
+                    )}
+                </Stack>
             )}
         </Box>
-    )
-}
+    );
+};
 
-
-export default BannerByUserID
-
+export default BannerByUserID;

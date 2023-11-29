b1760bd7a713cb6450cc6bb989845e7adf16ae12
yonathan <yonathan.eisenberg@gmail.com>
2023-11-29 15:12:28 +0200

    (add) login-Form-test, card-product-test

--- client/src/__tests__/Header.test.tsx
+++ client/src/__tests__/Header.test.tsx
@@ -1,9 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import Header from '../components/Header-Footer/Header';
+// import userEvent from '@testing-library/user-event';
+
+// const handleClickHomePage = vi.fn();
 
 describe('Header component', () => {
-  it('renders username correctly', () => {
+    it('renders username correctly', () => {
     localStorage.setItem('name', 'John Doe');
     render(
       <MemoryRouter>
@@ -12,9 +15,9 @@ describe('Header component', () => {
     );
     const usernameElement = screen.getByText(/Hello John Doe/i);
     expect(usernameElement).toBeInTheDocument();
-  });
+    });
 
-  it('renders default username when localStorage is empty', () => {
+    it('renders default username when localStorage is empty', () => {
     localStorage.removeItem('name');
     render(
       <MemoryRouter>
@@ -23,5 +26,13 @@ describe('Header component', () => {
     );
     const defaultUsernameElement = screen.getByText(/Hello User/i);
     expect(defaultUsernameElement).toBeInTheDocument();
-  });
-});
+    });
+// tests on the routing, meentime i dont know what the problem.
+    // it("button is clicked using userEvent", async () => {
+    // const user = userEvent.setup()
+    // render(<BrowserRouter><Header /></BrowserRouter>)
+    // const buttonElement = screen.getByRole("button")
+    // await user.click(buttonElement);
+    // expect(handleClickHomePage).toHaveBeenCalled()
+    // })
+})

71f8f5d8c89a9e9fee6492f93d1fd697463540d2
yonathan <yonathan.eisenberg@gmail.com>
2023-12-06 13:16:43 +0200

    (add) query get all uesrs

--- client/src/api/users/getUsersFetch.ts
+++ client/src/api/users/getUsersFetch.ts
@@ -1,29 +1,52 @@
-import axios from 'axios';
-const API_URL = import.meta.env.VITE_API_URI
+// import axios from 'axios';
+// const API_URL = import.meta.env.VITE_API_URI
 
 
-export const getAllUsers = async () => {
+// export const getAllUsers = async () => {
 
-    let data = '';
-    let token = localStorage.getItem('banner_token');
-    if (!token) { token = ''}
+//     let data = '';
+//     let token = localStorage.getItem('banner_token');
+//     if (!token) { token = ''}
     
     
-    let config = {
-        method: 'get',
-        maxBodyLength: Infinity,
-        url: `${API_URL}/users/`,
-        headers: { 
-            'Authorization': token, 
-            'Content-Type': 'application/json'
-          },
-        data: data
-    };
-    try {
-        const res = await axios.request(config)
-        return res.data
-    } catch (error) {
-        console.log(error);
+//     let config = {
+//         method: 'get',
+//         maxBodyLength: Infinity,
+//         url: `${API_URL}/users/`,
+//         headers: { 
+//             'Authorization': token, 
+//             'Content-Type': 'application/json'
+//           },
+//         data: data
+//     };
+//     try {
+//         const res = await axios.request(config)
+//         return res.data
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+import { useQuery, gql } from '@apollo/client';
+
+const GET_USERS = gql`
+  query {
+    users {
+      _id
+      name
+      email
+      password
+      isadmin
     }
-}
+  }
+`;
 
+const GetAllUsers = () => {
+  const { loading, error, data } = useQuery(GET_USERS);
+
+  if (loading) return "Loading...";
+  if (error) return error.message;
+  return data
+};
+
+export default GetAllUsers
